44e96749d53ab1ea2497cd45853765eb3ede7977
HarlingPI <BePJaP@outlook.com>
2024-09-28 18:17:13 +0800

    Commit by scripts

---
+++ GitLib/GitLib.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+public static class GitLib
+{
+    private static Regex fatal = new Regex("fatal|error", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+    public static void ExcuteCommand(string directory, string[] commands)
+    {
+        for (int i = 0; i < commands.Length; i++)
+        {
+            int count = 1;
+            var cmd = commands[i];
+
+            bool needretry = true;
+            while (needretry)
+            {
+                var startinfo = new ProcessStartInfo()
+                {
+                    FileName = "git",
+                    Arguments = cmd,
+                    WorkingDirectory=directory,
+                    // 重定向标准输出
+                    RedirectStandardOutput = true,
+                    // 重定向标准错误
+                    RedirectStandardError = true,
+                    // 不使用系统shell
+                    UseShellExecute = false,
+                    // 不创建窗口
+                    CreateNoWindow = true
+                };
+                // 启动进程
+                using (Process process = new Process { StartInfo = startinfo })
+                {
+                    process.Start();
+
+                    string output = null;
+                    // 读取标准输出
+                    output += process.StandardOutput.ReadToEnd();
+                    // 读取标准错误
+                    output += process.StandardError.ReadToEnd();
+                    // 等待进程完成
+                    process.WaitForExit();
+
+                    if (fatal.IsMatch(output))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(output);
+                        Console.WriteLine($"第{count++}次处理失败,失败命令{cmd},将开始尝试{count}次处理!");
+                        needretry = true;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine(output);
+                        needretry = false;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Push/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Push
+{
+    internal class Program
+    {
+        private static Regex moduleexp = new Regex("path = [\\w]+", RegexOptions.Compiled);
+        static void Main(string[] args)
+        {
+            string path = System.Environment.CurrentDirectory;
+            //先查找gitmodules文件
+            string mapper = path + "/.gitmodules";
+            List<string> modules = new List<string>();
+            if (File.Exists(mapper))
+            {
+                string content = File.ReadAllText(mapper);
+                foreach (Match item in moduleexp.Matches(content))
+                {
+                    modules.Add($"{path}/{item.Value[7..]}");
+                }
+            }
+            modules.Add(path);
+
+            Console.WriteLine($"已找到项目:");
+            for (int i = 0; i < modules.Count; i++)
+            {
+                Console.WriteLine(modules[i]);
+            }
+
+            string addcmd = "add .";
+            string commitcmd = "commit -m \"{0}\"";
+            string pushcmd = "push";
+
+            Console.WriteLine("请输入本次操作名称:");
+            string? optinename = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(optinename))
+            {
+                optinename = "Commit by scripts";
+            }
+            Console.WriteLine($"即将开始执行操作L{optinename}");
+            commitcmd = string.Format(commitcmd, optinename);
+            string[] commands = new[] { addcmd, commitcmd, pushcmd };
+
+            for (int i = 0; i < modules.Count; i++)
+            {
+                GitLib.ExcuteCommand(modules[i], commands);
+            }
+
+
+            Console.WriteLine("'push'已完成,请按任意键退出!");
+            Console.Read();
+        }
+    }
+}
