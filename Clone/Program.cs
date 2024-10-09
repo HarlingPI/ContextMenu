532f4be189e877a2e43f767d6038b808b7035020
HarlingPI <BePJaP@outlook.com>
2024-10-09 08:16:26 +0800

    Commit by scripts

---
+++ Clone/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Clone
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> modules = GitLib.SearchModules();
+
+            for (int i = 0; i < modules.Count; i++)
+            {
+                Console.WriteLine(modules[i]);
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("'clone'已完成,请按任意键退出!");
+            Console.Read();
+        }
+    }
+}
