914a512c9e070cb99e2ed47b50f10d4002ca6e8c
HarlingPI <BePJaP@outlook.com>
2025-05-20 22:26:46 +0800

    Pref   重构GitTool工作路径初始化流程

--- GitTool/Program.cs
+++ GitTool/Program.cs
@@ -8,17 +8,11 @@ namespace GitTool
     internal class Program
     {
         private static Dictionary<string, Command> allcmds = new Dictionary<string, Command>();
+        private static string working = "";
+        private static string[] projects;
         static void Main(string[] args)
         {
-            //获取工作路径与初始化命令字典
-            var working = InitCommands();
-            //查找当前目录下的所有Git项目
-            var projects = GitLib.FindProjects(working);
-            //输出所有找到的Git项目
-            for (int i = 0; i < projects.Length; i++)
-            {
-                Console.WriteLine(projects[i]);
-            }
+            InitProgram();
             while (true)
             {
                 //输出工作路径
@@ -43,6 +37,9 @@ namespace GitTool
                 else Environment.Exit(0);
             }
         }
+
+
+
         /// <summary>
         /// 命令分割正则表达式，空格分割，""中的空格不分割
         /// </summary>
@@ -55,12 +52,51 @@ namespace GitTool
                 .Where(w => !string.IsNullOrEmpty(w))
                 .ToArray();
         }
+        /// <summary>
+        /// 初始化程序工作目录与查找项目
+        /// </summary>
+        /// <param name="folder"></param>
+        public static void InitProgram(string folder = null)
+        {
+            //获取工作路径
+            working = SetAndGetWorkingFolder(folder);
+            //初始化所有指令
+            InitCommands(working);
+            //查找当前目录下的所有Git项目
+            projects = GitLib.FindProjects(working);
+            //输出所有找到的Git项目
+            for (int i = 0; i < projects.Length; i++)
+            {
+                Console.WriteLine(projects[i]);
+            }
+        }
 
+        private static string SetAndGetWorkingFolder(string folder = null)
+        {
+            var working = "";
+            if (string.IsNullOrWhiteSpace(folder))
+            {
+                working = AppDomain.CurrentDomain.BaseDirectory;
+                Directory.SetCurrentDirectory(working);
+            }
+            else
+            {
+                //target = Environment.ExpandEnvironmentVariables(folder);
+                // 解析相对路径
+                Directory.SetCurrentDirectory(Path.GetFullPath(folder));
+                working = Directory.GetCurrentDirectory();
+            }
+            return working;
+        }
 
-        private static string InitCommands()
+        /// <summary>
+        /// 初始化程序工作目录与查找项目
+        /// </summary>
+        /// <param name="folder"></param>
+        /// <returns></returns>
+        private static string InitCommands(string folder)
         {
             var cmdtype = typeof(Command);
-
             var types = cmdtype
                 .Assembly
                 .GetTypes()
@@ -68,14 +104,11 @@ namespace GitTool
                 .Where(t => !t.IsAbstract)
                 .ToArray();
 
-            //获取工作路径
-            var working = AppDomain.CurrentDomain.BaseDirectory;
-
             for (int i = 0; i < types.Length; i++)
             {
                 var type = types[i];
-                var command = (Command)Activator.CreateInstance(type, working);
-                allcmds.Add(type.Name.ToLower(), command);
+                var command = (Command)Activator.CreateInstance(type, folder);
+                allcmds[type.Name.ToLower()] = command;
             }
             return working;
         }
