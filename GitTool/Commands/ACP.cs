b267229e2f0d20be22a0e511cc4430e8f771d37e
HarlingPI <BePJaP@outlook.com>
2025-05-20 21:47:09 +0800

    Pref 将acp命令与push命令分离

--- GitTool/Commands/ACP.cs
+++ GitTool/Commands/ACP.cs
@@ -8,21 +8,33 @@ namespace GitTool.Commands
 {
     /// <summary>
     /// 作者:   Harling
-    /// 时间:   2025/5/18 10:31:21
+    /// 时间:   2025/5/20 21:33:17
     /// 备注:   此文件通过PIToolKit模板创建
     /// </summary>
     /// <remarks></remarks>
-    public class Push : Command
+    public class ACP : Command
     {
-        public Push(string workingFolder) : base(workingFolder)
+        public ACP(string workingFolder) : base(workingFolder)
         {
         }
 
         public override void Excute(string[] projects, params string[] args)
         {
+            var option = args.Where(a => a.StartsWith("\"") && a.EndsWith("\"")).FirstOrDefault();
+            if (string.IsNullOrWhiteSpace(option))
+            {
+                option = "\"Commit by scripts\"";
+            }
+            else
+            {
+                args = args.Where(a => a != option).ToArray();
+            }
+
+            var addcmd = "add .";
+            var commitcmd = $"commit -m {option}";
             var pushcmd = $"push {string.Join(" ", args)}";
 
-            var commands = new[] { pushcmd };
+            string[] commands = new[] { addcmd, commitcmd, pushcmd };
 
             for (int i = 0; i < projects.Length; i++)
             {
