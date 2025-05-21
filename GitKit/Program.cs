d8d27ac718674a9d042da66e25c7d183e0445bb1
HarlingPI <BePJaP@outlook.com>
2025-05-21 11:37:50 +0800

    Pref 优化命令与结果显示

--- GitKit/Program.cs
+++ GitKit/Program.cs
@@ -24,6 +24,7 @@ namespace GitKit
                 Console.ForegroundColor = orgcolor;
 
                 var typein = Console.ReadLine();
+                Console.WriteLine();
 
                 if (!string.IsNullOrEmpty(typein))
                 {
