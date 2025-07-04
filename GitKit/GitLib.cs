d077130cc13cff1a68a70e1cbcb5cdd12ab5a455
HarlingPI <BePJaP@outlook.com>
2025-07-04 09:33:01 +0800

    Pref 添加上级目录查找功能

--- GitKit/GitLib.cs
+++ GitKit/GitLib.cs
@@ -19,10 +19,21 @@ namespace GitKit
         private static Regex moduleexp = new Regex("path = [\\w]+", RegexOptions.Compiled);
         private static Regex errorexp = new Regex("fatal:|error:", RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
+        /// <summary>
+        /// 查找指定路径下的所有Git项目
+        /// </summary>
+        /// <param name="folder"></param>
+        /// <returns></returns>
         public static string[] FindProjects(string folder)
         {
             var projects = new List<string>();
             SearchSubProjects(folder, projects);
+            //如果向下没有找到任何Git项目，则向上查找Git项目
+            if (projects.IsNullOrEmpty())
+            {
+                var root = ExcuteGitCommand(folder, "rev-parse --show-toplevel", false);
+                projects.Add(root.Trim().Replace('/','\\'));
+            }
             return projects.ToArray();
         }
         private static void SearchSubProjects(string folder, List<string> result)
@@ -82,36 +93,26 @@ namespace GitKit
                 Console.WriteLine($"未知错误: {ex.Message}");
             }
         }
-
-        public static void ExcuteCommand(string directory, string command, uint retry = uint.MaxValue, bool setworkdir = true)
+        /// <summary>
+        /// 执行指令
+        /// </summary>
+        /// <param name="directory"></param>
+        /// <param name="command"></param>
+        /// <param name="retry"></param>
+        /// <param name="setworkdir"></param>
+        /// <returns></returns>
+        public static string ExcuteCommand(string directory, string command, uint retry = uint.MaxValue, bool setworkdir = true)
         {
             command = command.Trim();
             var orgcolor = Console.ForegroundColor;
 
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine($"\"{command}\":{directory}");
+            var output = "";
 
             for (int i = 0; i < retry; i++)
             {
-                var startinfo = new ProcessStartInfo()
-                {
-                    FileName = "git",
-                    Arguments = command,
-                    // 重定向标准输出
-                    RedirectStandardOutput = true,
-                    // 重定向标准错误
-                    RedirectStandardError = true,
-                    // 不使用系统shell
-                    UseShellExecute = false,
-                    // 不创建窗口
-                    CreateNoWindow = true
-                };
-                if (setworkdir)
-                {
-                    startinfo.WorkingDirectory = directory;
-                }
-                // 启动进程
-                StartProcess(startinfo, out var exitcode, out string output);
+                output = ExcuteGitCommand(directory, command, setworkdir);
 
                 if (/*exitcode != 0 || */errorexp.IsMatch(output))
                 {
@@ -130,10 +131,41 @@ namespace GitKit
                     break;
                 }
             }
-
             Console.ForegroundColor = orgcolor;
+            return output;
         }
 
+        private static string ExcuteGitCommand(string directory, string command, bool setworkdir = true)
+        {
+            string? output;
+            var startinfo = new ProcessStartInfo()
+            {
+                FileName = "git",
+                Arguments = command,
+                // 重定向标准输出
+                RedirectStandardOutput = true,
+                // 重定向标准错误
+                RedirectStandardError = true,
+                // 不使用系统shell
+                UseShellExecute = false,
+                // 不创建窗口
+                CreateNoWindow = true
+            };
+            if (setworkdir)
+            {
+                startinfo.WorkingDirectory = directory;
+            }
+            // 启动进程
+            StartProcess(startinfo, out var exitcode, out output);
+            return output;
+        }
+
+        /// <summary>
+        /// 开启一个进程并等待其完成
+        /// </summary>
+        /// <param name="startinfo"></param>
+        /// <param name="exitcode"></param>
+        /// <param name="output"></param>
         public static void StartProcess(ProcessStartInfo startinfo, out int exitcode, out string output)
         {
             output = "";
@@ -151,7 +183,11 @@ namespace GitKit
                 exitcode = process.ExitCode;
             }
         }
-
+        /// <summary>
+        /// 从Git仓库的URL中获取项目名称
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
         public static string GetProjectNameFromUrl(string? url)
         {
             string folder;
