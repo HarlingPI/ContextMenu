9330c8cb48121cac50a6b1526a8b65ec3ffa158b
HarlingPI <BePJaP@outlook.com>
2025-10-13 22:58:46 +0800

    Commit by scripts

---
+++ CustomTools/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+namespace CustomTools
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
