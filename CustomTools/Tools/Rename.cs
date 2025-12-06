72e7f3daf964e37cfc16aa86fb526dd4a8cee095
HarlingPI <BePJaP@outlook.com>
2025-12-06 19:27:59 +0800

    Commit by scripts

---
+++ CustomTools/Tools/Rename.cs
@@ -0,0 +1,28 @@
+using PIToolKit.Public.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomTools.Tools
+{
+    /// <summary>
+    /// 作者:   Harling
+    /// 时间:   2025/12/6 19:15:35
+    /// 备注:   此文件通过PIToolKit模板创建
+    /// </summary>
+    /// <remarks></remarks>
+    public class Rename : ITool
+    {
+        public Rename()
+        {
+
+        }
+
+        public void Process(string path)
+        {
+            
+        }
+    }
+}
