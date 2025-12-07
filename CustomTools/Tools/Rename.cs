88a5c473483bf61990faf07e1c44d06ec04d9404
HarlingPI <BePJaP@outlook.com>
2025-12-07 15:14:37 +0800

    Commit by scripts

--- CustomTools/Tools/Rename.cs
+++ CustomTools/Tools/Rename.cs
@@ -45,46 +45,47 @@ namespace CustomTools.Tools
             Ansi.ClearLastLine();
 
             if (search.Result.Length == 0) Console.WriteLine($"本次运行不处理任何文件!");
-            else
+            else RenameFiles(path, search.Result);
+        }
+
+        private static void RenameFiles(string path, (string file, string newname)[] files)
+        {
+            //隐藏光标
+            Ansi.HideCursor();
+            Console.Write($"任务进度:{Effects.ProgressBar(40, 0)}({1}/{files.Length})");
+
+            for (int i = 0; i < files.Length; i++)
             {
-                //隐藏光标
-                Ansi.HideCursor();
-                var files = search.Result;
-                Console.Write($"任务进度:{Effects.ProgressBar(40, 0)}({1}/{files.Length})");
-
-                for (int i = 0; i < files.Length; i++)
+                var (srcpath, newname) = files[i];
+                //重命名文件
+                //判断有没有目标文件
+                var tarfullpath = Path.Combine(FileUtils.GetDirectory(srcpath), newname);
+                var renameable = !FileUtils.FileIsExist(tarfullpath);
+                if (!renameable)
                 {
-                    var (srcpath, newname) = files[i];
-                    //重命名文件
-                    //判断有没有目标文件
-                    var tarfullpath = Path.Combine(FileUtils.GetDirectory(srcpath), newname);
-                    var renameable = !FileUtils.FileIsExist(tarfullpath);
-                    if (!renameable)
-                    {
-                        //如果已存在同名文件,判断hash值是否一致
-                        var srchash = FileUtils.GetHashValue(srcpath);
-                        var tarhash = FileUtils.GetHashValue(tarfullpath);
+                    //如果已存在同名文件,判断hash值是否一致
+                    var srchash = FileUtils.GetHashValue(srcpath);
+                    var tarhash = FileUtils.GetHashValue(tarfullpath);
 
-                        if (tarhash == srchash) renameable = true;
+                    if (tarhash == srchash) renameable = true;
 
-                        //如果hash值不一致，说明不是同一个文件,暂不处理
-                    }
-                    if (renameable) FileUtils.Rename(srcpath, newname, true);
-
-                    //清除上一次进度信息
-                    Ansi.ClearCurtLine();
-                    //输出文件名与重命名结果
-                    Console.Write($"{srcpath.Replace(path, ".")}\t{newname}");
-                    using (var scope = new ConsoleScope(renameable ? ConsoleColor.Green : ConsoleColor.Red))
-                    {
-                        Console.WriteLine($"\t{renameable}");
-                    }
-                    //更新进度条
-                    var counter = i + 1;
-                    Console.Write($"任务进度:{Effects.ProgressBar(40, counter / (float)files.Length)}({counter}/{files.Length})");
+                    //如果hash值不一致，说明不是同一个文件,暂不处理
                 }
-                Ansi.ShowCursor();
+                if (renameable) FileUtils.Rename(srcpath, newname, true);
+
+                //清除上一次进度信息
+                Ansi.ClearCurtLine();
+                //输出文件名与重命名结果
+                Console.Write($"{srcpath.Replace(path, ".")}\t{newname}");
+                using (var scope = new ConsoleScope(renameable ? ConsoleColor.Green : ConsoleColor.Red))
+                {
+                    Console.WriteLine($"\t{renameable}");
+                }
+                //更新进度条
+                var counter = i + 1;
+                Console.Write($"任务进度:{Effects.ProgressBar(40, counter / (float)files.Length)}({counter}/{files.Length})");
             }
+            Ansi.ShowCursor();
         }
 
         private (string file, string newname)[] SearchFile(string path)
